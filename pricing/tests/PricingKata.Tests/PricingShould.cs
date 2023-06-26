5e88f68ab0233917a001346b6adabf1c8aafc476
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-26 12:28:19 +0400

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 11
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- pricing/tests/PricingKata.Tests/PricingShould.cs
+++ pricing/tests/PricingKata.Tests/PricingShould.cs
@@ -4,12 +4,15 @@ namespace PricingKata.Tests
 {
     public class PricingShould
     {
-        [Fact]
-        public void Calculate_Price_Without_Tax() =>
-            Pricer.Calculate(3, 1.21m)
+        [Theory]
+        [InlineData(3, 1.21, 0, "3.63 €")]
+        public void Calculate(int numberOfItems, decimal unitPrice, decimal tax, string expectedResult)
+        {
+            Pricer.Calculate(numberOfItems, unitPrice, tax)
                 .Should()
-                .Be("3.63 €");
-        
+                .Be(expectedResult);
+        }
+
         [Fact]
         public void Calculate_Price_With_Tax_Of_5_Percent() =>
             Pricer.Calculate(3, 1.21m, 0.05m)
