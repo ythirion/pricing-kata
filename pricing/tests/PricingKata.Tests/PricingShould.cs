c588b9402d7a0d66aaa87a08941ad94af08852f6
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-26 12:03:33 +0400

    initial commit

---
+++ pricing/tests/PricingKata.Tests/PricingShould.cs
@@ -0,0 +1,11 @@
+namespace PricingKata.Tests
+{
+    public class PricingShould
+    {
+        [Fact]
+        public void Calculate_Price_Without_Tax()
+        {
+            Pricer.Calculate()
+        }
+    }
+}
