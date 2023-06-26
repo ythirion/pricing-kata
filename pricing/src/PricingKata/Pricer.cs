ac4830d566305a1f22282b155b3e45f627171640
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-26 12:37:53 +0400

    ✅ TCR - tests passing
    changed-lines:
        src: 19
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- pricing/src/PricingKata/Pricer.cs
+++ pricing/src/PricingKata/Pricer.cs
@@ -7,22 +7,21 @@ namespace PricingKata
         public static string Calculate(int numberOfItems, decimal unitPrice, decimal tax = 0)
         {
             var priceWithoutTax = numberOfItems * unitPrice;
-            var discountAmount = 0m;
-
-            if (priceWithoutTax >= 5000)
-            {
-                discountAmount = 0.05m * priceWithoutTax;
-            }
-            else if (priceWithoutTax >= 1000)
-            {
-                discountAmount = 0.03m * priceWithoutTax;
-            }
+            decimal discountAmount = DiscountAmount(priceWithoutTax);
 
             var priceWithDiscount = priceWithoutTax - discountAmount;
             var taxAmount = priceWithDiscount * tax;
 
             return (priceWithDiscount + taxAmount).ToEuroString();
         }
+
+        private static decimal DiscountAmount(decimal priceWithoutTax) =>
+            priceWithoutTax switch
+            {
+                >= 5000 => 0.05m,
+                >= 1000 => 0.03m,
+                _ => 0
+            }  * priceWithoutTax;
 
         private static string ToEuroString(this decimal totalAmount) =>
             string.Create(
