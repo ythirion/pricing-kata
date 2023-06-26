91d3d09b74e14a11056d835b453c177f88aebc79
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-26 12:43:05 +0400

    ✅ TCR - tests passing
    changed-lines:
        src: 13
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- pricing/src/PricingKata/Pricer.cs
+++ pricing/src/PricingKata/Pricer.cs
@@ -4,17 +4,28 @@ namespace PricingKata
 {
     public static class Pricer
     {
+        private static IReadOnlyDictionary<decimal, decimal> DisocuntBasedOnPrice = new Dictionary<decimal, decimal>()
+        {
+            {5000, 0.05m},
+            {1000, 0.03m}
+        };
+
         public static string Calculate(int numberOfItems, decimal unitPrice, decimal tax = 0)
         {
             var priceWithoutTax = PriceWithoutTax(numberOfItems, unitPrice);
-            var priceWithDiscount = priceWithoutTax - DiscountAmount(priceWithoutTax);
+            var priceWithDiscount = priceWithoutTax - DiscountAmount2(priceWithoutTax);
             var taxAmount = TaxAmount(tax, priceWithDiscount);
 
             return TotalAmount(priceWithDiscount, taxAmount)
                 .ToEuroString();
         }
-        
+
         private static decimal PriceWithoutTax(int numberOfItems, decimal unitPrice) => numberOfItems * unitPrice;
+
+        private static decimal DiscountAmount2(decimal priceWithoutTax) =>
+            DisocuntBasedOnPrice
+                .FirstOrDefault(x => priceWithoutTax >= x.Key)
+                .Value * priceWithoutTax;
 
         private static decimal DiscountAmount(decimal priceWithoutTax) =>
             priceWithoutTax switch
