167614a9a2276d65b53bdb591dca602b6ee23b6c
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-26 12:08:43 +0400

    first failing test

---
+++ pricing/src/PricingKata/Pricer.cs
@@ -0,0 +1,9 @@
+namespace PricingKata.Tests;
+
+public static class Pricer
+{
+    public static string Calculate(int numberOfItems, double unitPrice)
+    {
+        throw new NotImplementedException();
+    }
+}
--- pricing/tests/PricingKata.Tests/PricingShould.cs
+++ pricing/tests/PricingKata.Tests/PricingShould.cs
@@ -1,11 +1,13 @@
+using FluentAssertions;
+
 namespace PricingKata.Tests
 {
     public class PricingShould
     {
         [Fact]
-        public void Calculate_Price_Without_Tax()
-        {
-            Pricer.Calculate()
-        }
+        public void Calculate_Price_Without_Tax() =>
+            Pricer.Calculate(3, 1.21)
+                .Should()
+                .Be("3.63 €");
     }
 }
