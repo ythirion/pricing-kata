a628845825bf6b0aa6910d36537c30ebcaa7bdba
Yoan Thirion <yoan.thirion@gmail.com>
2023-06-26 12:43:22 +0400

    ✅ TCR - tests passing
    changed-lines:
        src: 16
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- pricing/src/PricingKata/Pricer.cs
+++ pricing/src/PricingKata/Pricer.cs
@@ -4,7 +4,7 @@ namespace PricingKata
 {
     public static class Pricer
     {
-        private static IReadOnlyDictionary<decimal, decimal> DisocuntBasedOnPrice = new Dictionary<decimal, decimal>()
+        private static IReadOnlyDictionary<decimal, decimal> DiscountBasedOnPrice = new Dictionary<decimal, decimal>()
         {
             {5000, 0.05m},
             {1000, 0.03m}
@@ -13,7 +13,7 @@ namespace PricingKata
         public static string Calculate(int numberOfItems, decimal unitPrice, decimal tax = 0)
         {
             var priceWithoutTax = PriceWithoutTax(numberOfItems, unitPrice);
-            var priceWithDiscount = priceWithoutTax - DiscountAmount2(priceWithoutTax);
+            var priceWithDiscount = priceWithoutTax - DiscountAmount(priceWithoutTax);
             var taxAmount = TaxAmount(tax, priceWithDiscount);
 
             return TotalAmount(priceWithDiscount, taxAmount)
@@ -22,19 +22,11 @@ namespace PricingKata
 
         private static decimal PriceWithoutTax(int numberOfItems, decimal unitPrice) => numberOfItems * unitPrice;
 
-        private static decimal DiscountAmount2(decimal priceWithoutTax) =>
-            DisocuntBasedOnPrice
+        private static decimal DiscountAmount(decimal priceWithoutTax) =>
+            DiscountBasedOnPrice
                 .FirstOrDefault(x => priceWithoutTax >= x.Key)
                 .Value * priceWithoutTax;
 
-        private static decimal DiscountAmount(decimal priceWithoutTax) =>
-            priceWithoutTax switch
-            {
-                >= 5000 => 0.05m,
-                >= 1000 => 0.03m,
-                _ => 0
-            }  * priceWithoutTax;
-        
         private static decimal TaxAmount(decimal tax, decimal priceWithDiscount) => priceWithDiscount * tax;
 
         private static decimal TotalAmount(decimal priceWithDiscount, decimal taxAmount) => priceWithDiscount + taxAmount;
